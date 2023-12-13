def8caaeff7e5e0f714f50548c42d8ca3e2e564b
Ihor Susoi <ihor.susoi@gmail.com>
2023-12-13 21:24:27 +0200

    add logic for externar repositories api

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -24,5 +24,26 @@ module Types
       response = http.request(request)
       response.body
     end
+
+    def get_user_repo_data(nickname)
+      user_data = send_to_external_repo_api(nickname)
+      result = JSON.parse(user_data)
+      result = result.map { |rep_info| rep_info['name'] }
+      {
+        name: result
+      }
+    end
+
+    def send_to_external_repo_api(nickname)
+      uri = URI.parse("https://api.github.com/users/#{nickname[:nickname]}/repos")
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = (uri.scheme == 'https')
+
+      request = Net::HTTP::Get.new(uri.path)
+      request.set_form_data('nickname' => nickname[:nickname])
+
+      response = http.request(request)
+      response.body
+    end
   end
 end
